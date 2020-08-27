5be8ccd182a5c2a8e242b33c25e3884d757d2c38
Nikitas Xanthopoulos <satikin.tnax@gmail.com>
2020-08-27 17:09:02 +0300

    obj loader working

--- ui/src/Augmented.js
+++ ui/src/Augmented.js
@@ -1,30 +1,38 @@
 import * as Three from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default class Augmented {
   constructor( properties ) {
     this.canvas = document.getElementById( 'obj-mesh' );
+    console.log(this.canvas)
+    
     this.initThree();
   }
   
   initThree() {
-    const aspectRatio=  window.innerWidth / window.innerHeight;
-    const light = new Three.PointLight(0xffffff, 1, 0);
+    const animate = () => {
+      requestAnimationFrame( animate );
+      this.renderer.render( this.scene, this.camera );
+    };
+
     this.scene = new Three.Scene();
-    this.camera = 
-      new Three.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
-    this.renderer = new Three.WebGLRenderer( { canvas: this.canvas } );
+    this.camera = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    this.renderer = new Three.WebGLRenderer();
+    const ambientLight = new Three.AmbientLight( 0xcccccc, 0.4 );
+    const pointLight = new Three.PointLight( 0xffffff, 0.8 );
+    const controls = new OrbitControls( this.camera, this.renderer.domElement )
     
-    this.camera.position.set(0, 0, 50);
+    this.renderer.setSize( window.innerWidth / 2, window.innerHeight / 2);
+    this.renderer.setClearColor(0xAFAFBF, 1);
+    document.body.appendChild( this.renderer.domElement );
     
-    light.position.set(1, 1, 1);
+    this.camera.position.z = 10;
     
-    this.scene.background = new Three.Color(0x111111);
-    this.scene.add(light);
-    
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-    this.renderer.render( this.scene, this.camera );
+    ambientLight.position.set( -10, -10, 10);
+    this.scene.add( ambientLight );
+    this.camera.add( pointLight );
+    animate();
   }
   
   showObj( show ) {
